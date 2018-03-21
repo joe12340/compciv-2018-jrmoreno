34e72a4543a7e5f42b9b1b6e179dcd5ef84a1a15
JosephJRM <jrmoreno@stanford.edu>
2018-03-21 14:32:13 -0700

    first

---
+++ week-02/foozbuzz/fooz.py
@@ -0,0 +1,14 @@
+def fob(input):
+n = 0
+while n < input:
+    if n % 3 == 0 and n % 5 == 0:
+        print(str (n) 'FizzBuzz')
+    if n % 3 == 0:
+        print(str (n)) 'Fizz')
+    if n % 5 == 0:
+        print(str (n) 'Buzz')
+    else:
+        print(str (n)
+    
+    n = n+1
+
